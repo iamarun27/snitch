d8ecd429d53c73d493215419c8870424aab6ee5c
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-09-25 17:09:48 +0530

    implement proxy in frontend

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -58,7 +58,7 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.bodyl;
+  const { email, password } = req.body;
   const user = await userModel.findOne({ email });
 
   if (!user) {
--- Frontend/src/features/auth/hook/useAuth.js
+++ Frontend/src/features/auth/hook/useAuth.js
@@ -1,6 +1,6 @@
 import { setError, setLoading, setUser } from "../state/auth.slice";
 
-import { register } from "../service/auth.api";
+import { register,login } from "../service/auth.api";
 import { useDispatch } from "react-redux";
 
 export const useAuth = () => {
